bd54995358f2b67c8249eee65846098dedd78bf8
neeeeeythan <jonathandavis.redoblado@gmail.com>
2023-02-06 04:28:17 +0800

    test2

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-fetch("users.json")
-
-.then(function(response){
-
-    return response.json();
-})
-
-.then(function(users){
-    let placeholder= document.querySelector("#data-output");
-    let out ="";
-    
-    for(let user of users){
-        out += `
-        <h4 class="user-name">${user.name}</h4>
-        <p class="user-email">Email: ${user.email}</p>
-        <p class="user-company">Company: ${user.company}</p>
-        <p class="user-address">Address: ${user.address}</p>
-        <p class="user-website">Website: ${user.website}</p>
-        <p class="user-age">Age: ${user.age}</p>
-        `;
-    }
-
-    placeholder.innerHTML = out;
-})
+$.getJSON('users.json', function(data) {
+    $.each(data.results, (user) => {
+            <div class="col-lg-4 d-flex">
+                <div class="user-container d-flex">
+                    <div class="col-lg-5">
+                        <div class="user-image-container">
+                            <img src="/district-manager/assets/userphoto.png" alt="User Photo" class="userphoto" />
+                        </div>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="user-info">
+                            <h4 class="user-name">${user.name}</h4>
+                            <p class="user-email">Email: ${user.email}</p>
+                            <p class="user-company">Company: ${user.company}</p>
+                            <p class="user-address">Address: ${user.address}</p>
+                            <p class="user-website">Website: ${user.website}</p>
+                            <p class="user-age">Age: ${user.age}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>;
+        })
+});
